6d71ecc5c4bf98fba8624a5152125a6a76d3f9f7
Hiroki.T <hirodora@me.com>
2019-03-04 23:13:01 +0900

    create: lib document

--- tmpgenlib.nim
+++ tmpgenlib.nim
@@ -1,8 +1,12 @@
 import os
 
-let template_dir = joinPath(getHomeDir(), ".tmpgen/")
+let template_dir = joinPath(getHomeDir(), ".tmpgen/")  ## save templates to template_dir
 
 proc isDir(filepath: string): bool =
+    ## Check is [filepath] directory?
+    ## Return:
+    ##     true or false
+
     let path_info: FileInfo = getFileInfo(filepath)
     if path_info.kind == pcDir:
         result = true
@@ -11,13 +15,12 @@ proc isDir(filepath: string): bool =
 
 
 proc fileCopy(from_filenames: seq[string], to_dir: string): void =
-    discard """Copy files from [from_filenames] to [to_dir]
-    Args:
-        from_filenames: string which includes regular expression
-        to_dir: copy to [to_dir]
-    """
-    if not existsDir(to_dir):
-        createDir(joinPath(to_dir))
+    ## Copy files from [from_filenames] to [to_dir]
+    ## Args:
+    ##     from_filenames: string which includes regular expression
+    ##     to_dir: copy to [to_dir]
+    ## if not existsDir(to_dir):
+    ##     createDir(joinPath(to_dir))
 
     for from_filename in from_filenames:  ## copy each files
         let to_path: string = joinPath(to_dir, from_filename.splitPath.tail)
@@ -29,33 +32,36 @@ proc fileCopy(from_filenames: seq[string], to_dir: string): void =
 
 
 proc initTemplateDir(label: string): void =
+    ## Delete files in [label] dir
+
     let template_dir = joinPath(template_dir, label)
     removeDir(template_dir)
 
 
 proc registTemplate*(files: seq[string], label: string): void =
-    discard """Regist files with label
-    if already exist template, there will be deleted.
-    Args:
-        files: filenames seq
-        label: template's label
-    """
+    ## Regist files with label
+    ## if already exist template, there will be deleted.
+    ## Args:
+    ##     files: filenames seq
+    ##     label: template's label
+
     initTemplateDir(label)
     fileCopy(files, joinPath(template_dir, label))
 
 
 proc addFilesToTemplate*(files: seq[string], label: string): void =
-    discard """Add files to template
-    if not exist template which has [label], create new template.
-    Args:
-        files: filenames seq
-        label: template's label
-    """
+    ## Add files to template
+    ## if not exist template which has [label], create new template.
+    ## Args:
+    ##     files: filenames seq
+    ##     label: template's label
+
     fileCopy(files, joinPath(template_dir, label))
 
 
 proc test: void =
     # argc and argv
+
     let
         argc = paramCount()
         argv = commandLineParams()
